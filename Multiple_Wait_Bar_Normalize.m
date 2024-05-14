3b0cf457f2ec10de422173c57a27ccebb5c599e5
Phy3hogga <alexander.hogg@ntu.ac.uk>
2024-05-14 13:20:12 +0100

    added double conversion to ensure no weirdness with fractional values.

--- Multiple_Wait_Bar_Normalize.m
+++ Multiple_Wait_Bar_Normalize.m
@@ -1,4 +1,5 @@
 %% Function to normalise an array between 0 and 1
 function Normalised_Array = Multiple_Wait_Bar_Normalize(Array)
+    Array = double(Array - min(Array(:)));
     Normalised_Array = Array ./ max(Array(:));
 end
