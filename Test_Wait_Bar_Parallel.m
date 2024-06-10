087faf2f7410b7947e811ca8da7a1b59ea6e5539
Phy3hogga <alexander.hogg@ntu.ac.uk>
2024-06-10 13:42:38 +0100

    Added Test_Wait_Bar_Parallel.m - Tests the parallel wait bar component.

---
+++ Test_Wait_Bar_Parallel.m
@@ -0,0 +1,24 @@
+%Setup
+clear all;
+
+%Set progressbar titles
+Progress(1).Title = 'i';
+Progress(2).Title = 'j';
+Progress(3).Title = 'k';
+%Set progressbar colours
+Progress(1).Colour = 'r';
+Progress(2).Colour = 'g';
+Progress(3).Colour = 'b';
+%Create progressbar figure
+Progress_Figure = Multiple_Wait_Bar(Progress);
+%Loop through all 3 progressbars
+for i = 0:0.2:1
+    Progress(1).Progress = i;
+    for j = 0:0.2:1
+        Progress(2).Progress = j;
+        parfor k = 0:0.1:1
+            %Progress(3).Progress = k;
+            %Progress_Figure = Multiple_Wait_Bar(Progress, Progress_Figure);
+        end
+    end
+end
